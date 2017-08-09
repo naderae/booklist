e86b33c1d20ac5cc8d903f5b61f304c6b83f0580
naderae <naderabouezze93@gmail.com>
2017-08-09 17:30:43 -0400

    done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,17 @@ class BooksController < ApplicationController
 
       respond_to do |format|
           format.html
+          format.text
+
+          format.csv do
+            render plain: Book.generate_csv(@books)
+          end
+
+          format.json do
+            render json: @books.map {|book|
+              {title: book.title }
+            }
+          end
       end
 
   end
